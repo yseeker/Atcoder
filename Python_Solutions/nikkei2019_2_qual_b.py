adbc5d655897707410265de39e7ed8a5ec5c49f6
Yu Saito <maxwell8313@gmail.com>
2020-01-26 22:01:37 -0800

    Update nikkei2019_2_qual_b.py

--- Python_Solutions/nikkei2019_2_qual_b.py
+++ Python_Solutions/nikkei2019_2_qual_b.py
@@ -28,35 +28,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-"""
-a = int(input())
-l = list(map(int,input().split()))
-d = [0]*a
-for i in l:
-    d[i]+=1
-r =1
-if  l[0]!=0:
-    print(0)
-    exit()
-for i in l[1:]:
-    r=r*d[i-1]%998244353
-print(r)
-
-
-import collections
-n=int(input())
-d=list(map(int,input().split()))
- 
-c=collections.Counter(d)
-ans=1
-ans=c[0]
- 
-if ans==1 and d[0]==0:
-    for i in range(1,len(c)):
-        ans*=(c[i-1]**c[i])%998244353
-    print(ans%998244353)
-else:
-    print(0)
-"""
